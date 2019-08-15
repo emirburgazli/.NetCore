4418d313b98c9b686229bcf431770a68afc17eec
Emir Burgazli <emirhanburgazli@gmail.com>
2019-08-15 12:09:02 +0300

    FirstCommit

--- FirstProject/Entities/Student.cs
+++ FirstProject/Entities/Student.cs
@@ -7,5 +7,9 @@ namespace FirstProject.Entities
 {
     public class Student
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
     }
 }
